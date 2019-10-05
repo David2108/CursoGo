4bf1807b5dc9118dbc3a9efd478c20c9e3b88010
unknown <deivid42@hotmail.es>
2019-10-05 18:47:53 -0500

    Curso Go v1.0

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const fullName string = "%s %s, David Vivanco"
+const edad = 29
+
+func main() {
+	var name string
+	hijos := 1
+	var estudio = "Superior"
+	var (
+		escuela     = "Escuela Juan Genaro Jaramillo"
+		colegio     = "Colegio Miguel de Santiago"
+		universidad = "Escuela Politécnica Nacional"
+	)
+	fmt.Println("Ingrese su nombre: ")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("Hola %s, ingreso.", name)
+	fmt.Println("Hijos: ", hijos)
+	fmt.Println("Estudios: ", estudio)
+	fmt.Println("Escuela: ", escuela)
+	fmt.Println("Colegio: ", colegio)
+	fmt.Println("Universidad: ", universidad)
+}
